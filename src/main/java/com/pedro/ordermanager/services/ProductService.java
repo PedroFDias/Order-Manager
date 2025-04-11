c5103126eb1561dcc826fffe3fecc7ad9a336404
PedroFDias <phf.dias.2022@gmail.com>
2025-04-11 18:24:30 -0300

    Adicionar dto para retorno do banco chamada response

--- src/main/java/com/pedro/ordermanager/services/ProductService.java
+++ src/main/java/com/pedro/ordermanager/services/ProductService.java
@@ -1,8 +1,6 @@
 package com.pedro.ordermanager.services;
 
-import com.pedro.ordermanager.dto.ProductCreateDTO;
-import com.pedro.ordermanager.dto.ProductDTO;
-import com.pedro.ordermanager.dto.ProductUpdateDTO;
+import com.pedro.ordermanager.dto.*;
 import com.pedro.ordermanager.model.Product;
 import com.pedro.ordermanager.repository.ProductRepository;
 import jakarta.validation.Valid;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,33 +17,37 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    public List<ProductDTO> get() {
-        return convertDatas(Optional.of(repository.findAll()));
+    public List<ProductResponseDTO> get() {
+        return convertData(Optional.of(repository.findAllByAvailableTrue()));
     }
-    public Page<ProductDTO> getPage(Pageable pageable) {
-        return repository.findAll(pageable).map(ProductDTO::new);
-    }
-
-    public List<ProductDTO> getTop5() {
-        return convertDatas(repository.findTop5ByOrderByPriceDesc());
+    public Page<ProductResponseDTO> getPage(Pageable pageable) {
+        return repository.findAllByAvailableTrue(pageable).map(ProductResponseDTO::new);
     }
 
-    public void post(@Valid ProductCreateDTO productDTO) {
+    public List<ProductResponseDTO> getTop5() {
+        return convertData(repository.findTop5ByOrderByPriceDesc());
+    }
+
+    public ProductCreateResponseDTO post(@Valid ProductCreateDTO productDTO) {
         repository.save(new Product(productDTO));
+        return new ProductCreateResponseDTO(productDTO);
     }
 
-    public void update(ProductUpdateDTO productDTO){
+    public ProductResponseDTO update(ProductUpdateDTO productDTO){
         var product = repository.getReferenceById(productDTO.id());
         product.updateProduct(productDTO);
+        return new ProductResponseDTO(product);
     }
 
-    private List<ProductDTO> convertDatas(Optional<List<Product>> products) {
+    private List<ProductResponseDTO> convertData(Optional<List<Product>> products) {
         return products.map(productList -> productList.stream()
-                .map(ProductDTO::new)
+                .map(ProductResponseDTO::new)
                 .collect(Collectors.toList())).orElse(null);
     }
 
-    public void delete(Long id) {
-        repository.deleteById(id);
+    public ProductResponseDTO delete(Long id) {
+        var product = repository.getReferenceById(id);
+        product.delete();
+        return new ProductResponseDTO(product);
     }
 }
