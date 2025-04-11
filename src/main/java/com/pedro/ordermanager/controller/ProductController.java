5d592a3636aedbdc2be6fcf50d40edf35e6e1ba7
PedroFDias <phf.dias.2022@gmail.com>
2025-04-11 18:25:45 -0300

    Criar respostas adequadas para cada requisição de produto

--- src/main/java/com/pedro/ordermanager/controller/ProductController.java
+++ src/main/java/com/pedro/ordermanager/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.pedro.ordermanager.controller;
 
 import com.pedro.ordermanager.dto.ProductCreateDTO;
-import com.pedro.ordermanager.dto.ProductDTO;
+import com.pedro.ordermanager.dto.ProductResponseDTO;
 import com.pedro.ordermanager.dto.ProductUpdateDTO;
-import com.pedro.ordermanager.model.Product;
 import com.pedro.ordermanager.services.ProductService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,36 +21,42 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public List<ProductDTO> getProducts() {
-        return service.get();
+    public ResponseEntity<List<ProductResponseDTO>> getProducts() {
+        var products = service.get();
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("page")
-    public Page<ProductDTO> getProductsPage(Pageable pageable) {
-        return service.getPage(pageable);
+    public ResponseEntity<Page<ProductResponseDTO>> getProductsPage(Pageable pageable) {
+        var pages = service.getPage(pageable);
+        return ResponseEntity.ok(pages);
     }
 
     @GetMapping("/top5")
-    public List<ProductDTO> top5MoreExpensive(){
-        return service.getTop5();
+    public ResponseEntity<List<ProductResponseDTO>> top5MoreExpensive(){
+        var products = service.getTop5();
+        return ResponseEntity.ok(products);
     }
 
     @PostMapping
     @Transactional
-    public void registerProduct(@RequestBody @Valid ProductCreateDTO data){
-        service.post(data);
+    public ResponseEntity registerProduct(@RequestBody @Valid ProductCreateDTO data){
+        var response = service.post(data);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid ProductUpdateDTO data){
-        service.update(data);
+    public ResponseEntity update(@RequestBody @Valid ProductUpdateDTO data){
+        var response = service.update(data);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
-        service.delete(id);
+    public ResponseEntity delete(@PathVariable Long id){
+        var data = service.delete(id);
+        return ResponseEntity.ok(data);
     }
 
 }
