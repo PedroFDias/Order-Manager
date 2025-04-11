4827b0bfd77232ee8febda4709e3690724bc2b8a
PedroFDias <phf.dias.2022@gmail.com>
2025-04-11 18:23:30 -0300

    Adicionar nova Query para dar get apenas nos produtos ativos

--- src/main/java/com/pedro/ordermanager/repository/ProductRepository.java
+++ src/main/java/com/pedro/ordermanager/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.pedro.ordermanager.repository;
 
 import com.pedro.ordermanager.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<List<Product>> findByPartOfTheName(String part);
 
     Optional<List<Product>> findTop5ByOrderByPriceDesc();
+
+    List<Product> findAllByAvailableTrue();
+    Page<Product> findAllByAvailableTrue(Pageable pageable);
 }
