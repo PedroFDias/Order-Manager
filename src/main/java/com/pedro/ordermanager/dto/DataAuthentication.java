450cc9d288fa9367482bab559da0117e51b404b8
PedroFDias <phf.dias.2022@gmail.com>
2025-06-05 16:04:09 -0300

    feat: dto's para respostas do spring

---
+++ src/main/java/com/pedro/ordermanager/dto/DataAuthentication.java
@@ -0,0 +1,4 @@
+package com.pedro.ordermanager.dto;
+
+public record DataAuthentication(String username, String password) {
+}
---
+++ src/main/java/com/pedro/ordermanager/dto/DataTokenJWT.java
@@ -0,0 +1,4 @@
+package com.pedro.ordermanager.dto;
+
+public record DataTokenJWT(String token) {
+}
